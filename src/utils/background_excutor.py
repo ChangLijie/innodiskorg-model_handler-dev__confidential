dfc33e3ebfb04771b045fef9a06e8a1912f610d0
ChangJay0212 <lastlife0212@gmail.com>
2025-01-16 16:53:51 +0800

    fix : use coroutine to lunch background task.

--- src/utils/background_excutor.py
+++ src/utils/background_excutor.py
@@ -10,12 +10,15 @@ class TaskExecutor:
 
     def run_in_background(self, task: Callable, *args: Any, **kwargs: Any):
         try:
-            if asyncio.iscoroutinefunction(task):
-                self.loop.run_in_executor(
-                    self.executor, lambda: asyncio.run(task(*args, **kwargs))
-                )
-            else:
-                self.loop.run_in_executor(self.executor, lambda: task(*args, **kwargs))
+            # if asyncio.iscoroutinefunction(task):
+            #     self.loop.run_in_executor(
+            #         self.executor, lambda: asyncio.run(task(*args, **kwargs))
+            #     )
+            # else:
+            #     self.loop.run_in_executor(self.executor, lambda: task(*args, **kwargs))
+            self.loop.run_in_executor(
+                self.executor, lambda: asyncio.run(task(*args, **kwargs))
+            )
         except Exception as e:
             raise Exception(e)
 
